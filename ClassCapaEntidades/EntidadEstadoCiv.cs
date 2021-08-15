93bcfc726dbe9685b1ccb37a2d6aa604d1e73d45
KPaolaL <86784313+KPaolaL@users.noreply.github.com>
2021-08-14 20:41:12 -0500

    02.Creacion de entidades de bitacora

---
+++ ClassCapaEntidades/EntidadEstadoCiv.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCapaEntidades
+{
+    public class EntidadEstadoCiv
+    {
+        public int idEdo { set; get; }
+        public string Estado { set; get; }
+        public string Extra { set; get; }
+    }
+}
