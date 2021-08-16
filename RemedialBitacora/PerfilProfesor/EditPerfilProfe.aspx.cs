3ec570660a9554d5a1261390ce4e9768b0c917f6
KPaolaL <86784313+KPaolaL@users.noreply.github.com>
2021-08-16 06:50:34 -0500

    22.Ultimas modificacion del crud del profesor

--- RemedialBitacora/PerfilProfesor/EditPerfilProfe.aspx.cs
+++ RemedialBitacora/PerfilProfesor/EditPerfilProfe.aspx.cs
@@ -27,46 +27,64 @@ namespace RemedialBitacora.PerfilProfesor
             if (!IsPostBack)
             {
                 List<EntidadPerfilProfe> mostrarPerf = null;
-                string msj = "";
                 string id = Convert.ToString(Session["id_seleccionado"]);
-                //mostrarPerf = LogPer.ObtenerPerfil(ref msj);
+              // mostrarPerf = LogPer.ObtenerPerfil(ref msj);
                 if (mostrarPerf != null)
                 {
-                    //foreach (EntidadPerfilProfe perfil in mostrarPerf)
+                  // foreach (EntidadPerfilProfe perfil in mostrarPerf)
                     {
-                       // TextBox1.Text = perfil.Estado;
-                       // TextBox2.Text = perfil.Evidencia;
-                     //   Calendar1 = perfil.FechaObtencion;
+                        // TextBox1.Text = perfil.Estado;
+                        // TextBox2.Text = perfil.Evidencia;
+                        //   Calendar1 = perfil.FechaObtencion;
 
                     }
                 }
-                List<EntidadProfesor> perfil = null;
-                perfil = LogPer.GetProfe(ref msj);
-                if (perfil != null)
+            
+                List<EntidadProfesor> feS = null;
+               // feS = LogPer.GetProfe(ref msj);
+                if (feS != null)
                 {
-                    foreach (EntidadProfesor edo in perfil)
+                    foreach (EntidadProfesor edo in feS)
                     {
                         DropDownList1.Items.Add(new ListItem(edo.Nombre.ToString(), edo.id_Profe.ToString()));
                         DropDownList1.DataBind();
                     }
                 }
-                //List<EntidadGradoEspecialidad> perfil = null;
-               // string msj = "";
-                //perfil = LogPer.GetGrado(ref msj);
+            
+                List<EntidadGradoEspecialidad> perfil = null;
+                string msj = "";
+                perfil = LogPer.GetGrado(ref msj);
                 if (perfil != null)
                 {
-                   // foreach (EntidadGradoEspecialidad edo in perfil)
+                    foreach (EntidadGradoEspecialidad edo in perfil)
                     {
-                     //   DropDownList2.Items.Add(new ListItem(edo.Titulo.ToString(), edo.id_Grado.ToString()));
+                        DropDownList2.Items.Add(new ListItem(edo.Titulo.ToString(), edo.id_Grado.ToString()));
                         DropDownList2.DataBind();
 
                     }
+
                 }
             }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            EntidadPerfilProfe temp = new EntidadPerfilProfe
+            {
+                F_Profe = Convert.ToInt16(DropDownList1.SelectedValue),
+                F_Grado = Convert.ToInt16(DropDownList2.SelectedValue),
+                Estado = TextBox1.Text,
+                FechaObtencion = Calendar1.SelectedDate,
+                Evidencia = TextBox2.Text
+            };
+
+            string resp = "";
+            Boolean recibe = false;
+            string id = Convert.ToString(Session["id_seleccionado"]);
+            recibe = LogPer.UpdatePerfil(temp, id, ref resp);
+            //string mensaje = "";
+            Server.Transfer("AgregaProfesor.aspx");
+
 
         }
     }
